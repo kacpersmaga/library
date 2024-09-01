c7a323268a97bb34cb2c172fb7513ab09e9e359e
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-09-01 19:15:48 +0200

    style: make the UI

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title , author , pages, isRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+
+}
+
+function addBookToLibrary(newBook){
+    myLibrary.push(newBook);
+
+}
+
+
