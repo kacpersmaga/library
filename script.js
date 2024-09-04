e80022bc2cdb43f7ca888432dc07127b2ad52661
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-09-04 13:56:18 +0200

    feat: add books, remove books

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const myLibrary = [];
+let myLibrary = [];
 
 const booksGrid = document.querySelector('main');
+const addBookBtn = document.querySelector('#add-book');
+const addBookModal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const addBookForm = document.querySelector('.add-book-form');
 
-function Book(title , author , pages, isRead){
+
+
+
+function Book(title , author , pages, isRead = 0){
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -14,6 +21,8 @@ function addBookToLibrary(newBook){
     myLibrary.push(newBook);
 
 }
+
+
 
 const createBookCard = (book) => {
     const bookCard = document.createElement('div');
@@ -28,6 +37,8 @@ const createBookCard = (book) => {
     buttonGroup.classList.add('button-group');
     readBtn.classList.add('btn');
     removeBtn.classList.add('btn');
+
+    removeBtn.onclick = () => removeBookFromLibrary(book);
 
     title.textContent = `"${book.title}"`;
     author.textContent = book.author;
@@ -51,6 +62,59 @@ const createBookCard = (book) => {
 
 }
 
+const openAddBookModal = () => {
+    addBookModal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+const closeAddBookModal = () => {
+    addBookModal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+const getBookInformation = () => {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const isRead = document.querySelector('#is-read').checked ? 1 : 0;
+    console.log({ title, author, pages, isRead });
+    return new Book(title,author,pages,isRead);
+}
+
+const addBook = (event) => {
+    event.preventDefault();
+    const newBook = getBookInformation();
+    addBookToLibrary(newBook);
+    createBookCard(newBook);
+    addBookForm.reset();
+    closeAddBookModal();
+}
+
+const resetBooksGrid = () =>{
+    booksGrid.innerHTML = '';
+}
+
+const updateBooksGrid = () => {
+    resetBooksGrid();
+    for(let book of myLibrary){
+        createBookCard(book);
+    }
+}
+
+function removeBookFromLibrary(bookToRemove){
+    myLibrary = myLibrary.filter((book) => book !== bookToRemove );
+    updateBooksGrid();
+}
+
+addBookBtn.onclick = openAddBookModal;
+overlay.onclick = closeAddBookModal;
+addBookForm.onsubmit = addBook;
+
 const newBook = new Book('xd','xd','xd','Read');
 createBookCard(newBook);
+
+myLibrary.forEach(element => {
+    console.log(element);
+    
+});
 
